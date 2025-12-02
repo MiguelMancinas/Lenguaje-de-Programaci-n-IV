e6412198b71c02dcf75f5a97d4f621b33a1e54c1
MiguelMancinas <167241677+MiguelMancinas@users.noreply.github.com>
2025-12-01 19:47:39 -0600

    Add files via upload

---
+++ Activiad_2Umi/src/activiad_2umi/Activiad_2Umi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activiad_2umi;
+
+/**
+ *
+ * @author dauzh
+ */
+public class Activiad_2Umi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Activiad_2Umi/src/activiad_2umi/Banco.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package activiad_2umi;
+
+public class Banco {
+    public static double saldo = 0;
+}
+
+/**
+ *
+ * @author dauzh
+ */
+    
